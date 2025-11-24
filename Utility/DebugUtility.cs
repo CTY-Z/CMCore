8e3ba6eba7a15b878a04e6c7f3be27b15e7c593b
Ctyz <1124981410@qq.com>
2025-11-24 21:20:57 +0800

    add namespace
    - DebugUtility

--- Utility/DebugUtility.cs
+++ Utility/DebugUtility.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-public static class DebugUtility
+namespace CMFramework.Core
 {
-    [Conditional("UNITY_EDITOR")]
-    public static void Log(object message)
+    public static class DebugUtility
     {
-        UnityEngine.Debug.Log(message);
-    }
+        [Conditional("UNITY_EDITOR")]
+        public static void Log(object message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
 
-    [Conditional("UNITY_EDITOR")]
-    public static void Warning(object message)
-    {
-        UnityEngine.Debug.LogWarning(message);
-    }
+        [Conditional("UNITY_EDITOR")]
+        public static void Warning(object message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
 
-    [Conditional("UNITY_EDITOR")]
-    public static void Error(object message)
-    {
-        UnityEngine.Debug.LogError(message);
-    }
+        [Conditional("UNITY_EDITOR")]
+        public static void Error(object message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
 
-    [Conditional("UNITY_EDITOR")]
-    public static void Ensure(bool flag, object message)
-    {
-        if (flag) Error(message);
+        [Conditional("UNITY_EDITOR")]
+        public static void Ensure(bool flag, object message)
+        {
+            if (flag) Error(message);
+        }
     }
 }
